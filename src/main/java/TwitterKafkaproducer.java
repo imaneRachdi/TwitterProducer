6ba2fc5f643c3beb06115f0e3520bbeb618a1117
RACHDI Imane <imane.rachdi2@gmail.com>
2016-12-23 16:18:20 +0100

    Modif

--- src/main/java/TwitterKafkaproducer.java
+++ src/main/java/TwitterKafkaproducer.java
@@ -10,6 +10,7 @@ import java.util.concurrent.LinkedBlockingQueue;
 
 import kafka.producer.KeyedMessage;
 import kafka.producer.ProducerConfig;
+import com.twitter.hbc.core.endpoint.Location;
 
 import com.google.common.collect.Lists;
 import com.twitter.hbc.ClientBuilder;
@@ -19,15 +20,16 @@ import com.twitter.hbc.core.endpoint.StatusesFilterEndpoint;
 import com.twitter.hbc.core.processor.StringDelimitedProcessor;
 import com.twitter.hbc.httpclient.auth.Authentication;
 import com.twitter.hbc.httpclient.auth.OAuth1;
+import org.apache.flink.api.java.utils.ParameterTool;
 
 public class TwitterKafkaproducer {
 
 
 	public static void run(String consumerKey, String consumerSecret,
-			String token, String secret,String topic) throws InterruptedException {
+			String token, String secret,String topic,String broker_list) throws InterruptedException {
 
 		Properties properties = new Properties();
-		properties.put("metadata.broker.list", "localhost:9092");
+		properties.put("metadata.broker.list", broker_list);
 		properties.put("serializer.class", "kafka.serializer.StringEncoder");
 		properties.put("client.id","camus");
 		ProducerConfig producerConfig = new ProducerConfig(properties);
@@ -37,9 +39,15 @@ public class TwitterKafkaproducer {
 		BlockingQueue<String> queue = new LinkedBlockingQueue<String>(10000);
 		StatusesFilterEndpoint endpoint = new StatusesFilterEndpoint();
 		// add some track terms
-		endpoint.trackTerms(Lists.newArrayList("twitterapi",
-				"#AAPSweep"));
+		// endpoint.trackTerms(Lists.newArrayList("Paris","London","New York"));
 
+endpoint.locations(Lists.newArrayList(new Location(new Location.Coordinate(-73.935242,40.730610),new Location.Coordinate(-74.935242,41.730610) )));
+//Paris
+               endpoint.locations(Lists.newArrayList( new Location(new Location.Coordinate(2.294694,48.858093),new Location.Coordinate(2.19,49) )));
+                //london
+        endpoint.locations(Lists.newArrayList(  new Location(new Location.Coordinate(-0.076132,51.508530),new Location.Coordinate(0.75,51.30) )));
+                       // new Location.Coordinate(2.294694,48.858093),
+                        //new Location.Coordinate(	-0.076132,51.508530	) ) ;
 		Authentication auth = new OAuth1(consumerKey, consumerSecret, token,
 				secret);
 		// Authentication auth = new BasicAuth(username, password);
@@ -53,8 +61,7 @@ public class TwitterKafkaproducer {
 		client.connect();
 
 		// Do whatever needs to be done with messages
-		for (int msgRead = 0; msgRead < 1000; msgRead++) {
-			KeyedMessage<String, String> message = null;
+while(true)		{	KeyedMessage<String, String> message = null;
 			try {
 				message = new KeyedMessage<String, String>(topic, queue.take());
 			} catch (InterruptedException e) {
@@ -62,10 +69,10 @@ public class TwitterKafkaproducer {
 			}
 			producer.send(message);
 		}
-		producer.close();
-		client.stop();
 
 	}
+
+
 
 	
 }
